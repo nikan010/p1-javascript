1ed7412c89a74b6a7b1ea3340645b87ca4f1fa48
nikan010 <9022105@student.zadkine.nl>
2023-10-05 09:09:17 +0200

    aa

--- javascript6.1/style.js
+++ javascript6.1/style.js
@@ -2,4 +2,4 @@ function kleuraanpassen() {
     document.getElementById("h1_opdracht").style.color = "#ff0000";
     document.getElementById("p_opdracht").innerHTML = "New Text!";
    }
- 
+   kleuraanpassen();
--- javascript6.2/style.js
+++ javascript6.2/style.js
@@ -2,4 +2,3 @@ function kleuraanpassen() {
     document.getElementById("h1_opdracht").style.color = "#ff0000";
     document.getElementById("p_opdracht").innerHTML = "New Text!";
    }
- 
