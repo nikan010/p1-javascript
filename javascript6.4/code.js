139c08072f461042a5fa3e1cba3719723e690bbd
nikan010 <9022105@student.zadkine.nl>
2023-10-06 11:08:57 +0200

    ss

---
+++ javascript6.4/code.js
@@ -0,0 +1,24 @@
+function calculate(getal1, getal2)
+{
+    antwoord = getal1 + getal2;
+    console.log("het antword van de som is ");
+    console.log(antwoord);
+
+}
+calculate(5, 5);
+
+function multiplyByFive(getal1)
+{
+    antwoord = getal1 * 5;
+    console.log("het antwoord op de keer som is");
+    console.log(antwoord);
+}
+multiplyByFive(5);
+
+function calculateminutes(seconden)
+{
+    antwoord = seconden / 60;
+    console.log("het antwoord op de som is");
+    console.log(antwoord);
+}
+calculateminutes(600);
