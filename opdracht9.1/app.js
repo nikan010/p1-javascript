300b56b5447b1a4d8258b040cf237e2af84e2b2a
nikan010 <9022105@student.zadkine.nl>
2023-11-13 12:55:07 +0100

    sss

--- opdracht9.1/app.js
+++ opdracht9.1/app.js
@@ -10,10 +10,9 @@ function showModal() {
     var leeftijd = document.getElementById('leeftijdInput').value;
 
     if (leeftijd !== '' && parseInt(leeftijd) >= 18) {
-      // Ga naar een website naar keuze (bijvoorbeeld Google)
       window.location.href = 'https://www.google.com';
     } else {
-      // Laat een rode pagina zien met de tekst "helaas je bent te jong"
+     
       document.body.innerHTML = '<div style="text-align: center; color: red; padding: 50px;"><h1>Helaas, je bent te jong!</h1></div>';
     }
   }
