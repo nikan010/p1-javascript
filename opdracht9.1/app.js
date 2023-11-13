24df1db315957e5d09d3353ee1ed150cccc74098
nikan010 <9022105@student.zadkine.nl>
2023-11-13 12:53:24 +0100

    sss

--- opdracht9.1/app.js
+++ opdracht9.1/app.js
@@ -1,23 +1,19 @@
-var modal = document.getElementById("jevader");
-
-
-var btn = document.getElementById("knop");
-
-var span = document.getElementsByClassName("jemoeder")[0];
-
-
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+function showModal() {
+    document.getElementById('myModal').style.display = 'block';
   }
-}
+
+  function closeModal() {
+    document.getElementById('myModal').style.display = 'none';
+  }
+
+  function checkLeeftijd() {
+    var leeftijd = document.getElementById('leeftijdInput').value;
+
+    if (leeftijd !== '' && parseInt(leeftijd) >= 18) {
+      // Ga naar een website naar keuze (bijvoorbeeld Google)
+      window.location.href = 'https://www.google.com';
+    } else {
+      // Laat een rode pagina zien met de tekst "helaas je bent te jong"
+      document.body.innerHTML = '<div style="text-align: center; color: red; padding: 50px;"><h1>Helaas, je bent te jong!</h1></div>';
+    }
+  }
